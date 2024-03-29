eb16e9f2b12b49591cb41bc0e923b21e2e6c1716
robmi10 <89600540+robmi10@users.noreply.github.com>
2024-03-29 14:49:58 +0100

    tweaks

--- app/reminder/index.tsx
+++ app/reminder/index.tsx
@@ -1,14 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { RxPencil1 } from "react-icons/rx";
-import { CiCircleRemove, CiTrash } from "react-icons/ci";
+import { CiTrash } from "react-icons/ci";
 import { IoCheckmark } from "react-icons/io5";
 import Buttonanimate from '../components/animation/buttonanimate';
 import {
     Dialog,
     DialogContent,
     DialogDescription,
-    DialogHeader,
-    DialogTitle,
     DialogTrigger,
 } from "@/components/ui/dialog"
 import { Button } from '@/components/ui/button';
@@ -18,10 +16,16 @@ import { IoCloseOutline } from "react-icons/io5";
 import { format } from "date-fns";
 
 const Reminder = () => {
+    const [remindersObj, setRemindersObj] = useState({
+        desc: '',
+        reminder: '',
+        start: '',
+
+    })
     const useReminders = api.ai.getUserReminders.useQuery({ userId: 1 })
     const reminderStatusMutation = api.ai.setReminderStatus.useMutation({
         onSettled() {
-            console.log("it settled now!")
+            useReminders.refetch()
         }
     })
     const deleteReminderMutation = api.ai.deleteReminder.useMutation({
@@ -29,6 +33,7 @@ const Reminder = () => {
             console.log("it deleted now!")
         }
     })
+
 
     const handleSetReminderStatus = (eventId: number | undefined, status: boolean) => {
         if (!eventId) return null
@@ -52,7 +57,26 @@ const Reminder = () => {
         })
     }
 
+    const handleEditReminder = (eventId: number | undefined) => {
+        const startTime = new Date(`1970-01-01T${remindersObj.start}:00`);
+        const reminderTime = new Date(`1970-01-01T${remindersObj.reminder}:00`);
+
+        console.log("eventId check ->", eventId)
+        console.log("desc check ->", remindersObj.desc)
+        console.log("start check ->", remindersObj.start)
+        console.log("reminder check ->", remindersObj.reminder)
+
+        if (reminderTime > startTime) {
+            console.log("Reminder is later than start time.");
+        } else if (reminderTime < startTime || reminderTime === startTime) {
+            console.log("Reminder is earlier than start time or the same.");
+        }
+
+
+    }
+
     const reminders = useReminders.data && useReminders?.data
+
     return (
         <div className='h-full w-full flex items-center justify-center flex-col gap-12'>
             <div className='text-2xl font-medium w-full flex items-center justify-center'>Reminders</div>
@@ -63,54 +87,55 @@ const Reminder = () => {
 
             <div className='place-items-center w-2/4 h-full grid grid-cols-2 gap-8 '>
                 {reminders && reminders.map((opts) => {
-                    const startDate = format(new Date(opts.start), "yyyy-MM-dd");
-                    const reminderDate = format(new Date(opts.start), "yyyy-MM-dd HH:mm");
-
+                    const startDate = format(new Date(opts.start), "yyyy-MM-dd HH:mm");
+                    const reminderDate = format(new Date(opts.reminder), "yyyy-MM-dd HH:mm");
                     return (
                         <div className='w-full bg-white h-54 p-8 gap-2 flex flex-col shadow-lg rounded-xl relative'>
                             <div className='absolute right-2 justify-end flex'>
                                 <div className='flex gap-2'>
-                                    <>
-                                        <Dialog>
-                                            <DialogTrigger asChild className='w-full relative'>
-                                                <RxPencil1 className='cursor-pointer absolute w-full right-0 top-0' />
-                                            </DialogTrigger>
-                                            <DialogContent className='bg-white rounded-xl p-8 shadow-2xl'>
-                                                <span className='text-xl'>Edit Reminder.</span>
+                                    <Dialog>
+                                        <DialogTrigger asChild className='w-full relative'>
+                                            <RxPencil1 className='cursor-pointer absolute w-full right-0 top-0' />
+                                        </DialogTrigger>
+                                        <DialogContent className='bg-white rounded-xl p-8 shadow-2xl'>
+                                            <span className='text-xl'>Edit Reminder</span>
 
-                                                <div>
-                                                    <span className='text-sm'>Description</span>
-                                                    <Input type='text' placeholder="Desc" className='rounded-3xl border-stone-200 shadow-3xl text-stone-400' />
-                                                </div>
+                                            <div>
+                                                <span className='text-sm'>Description</span>
+                                                <Input value={remindersObj.desc} onChange={(e) => { setRemindersObj(current => ({ ...current, desc: e.target.value })) }} type='text' placeholder="Desc" className='rounded-3xl border-stone-200 shadow-3xl placeholder:text-stone-400' />
+                                            </div>
 
-                                                <div>
-                                                    <span className=' text-sm'>Task date</span>
-                                                    <Input type='date' placeholder="Task date" className='rounded-3xl border-stone-200 shadow-3xl text-stone-400' />
-                                                </div>
+                                            <div>
+                                                <span className=' text-sm'>Task time</span>
+                                                <Input value={remindersObj.start} onChange={(e) => { setRemindersObj(current => ({ ...current, start: e.target.value })) }} type='time' placeholder="Task time" className='rounded-3xl border-stone-200 shadow-3xl placeholder:text-stone-400' />
+                                            </div>
 
-                                                <div>
-                                                    <span className=' text-sm'>Reminder</span>
-                                                    <Input type='time' placeholder="Reminder" className='rounded-3xl border-stone-200 shadow-3xl text-stone-400' />
-                                                </div>
-                                            </DialogContent>
-                                        </Dialog>
+                                            <div>
+                                                <span className=' text-sm'>Reminder time</span>
+                                                <Input value={remindersObj.reminder} onChange={(e) => { setRemindersObj(current => ({ ...current, reminder: e.target.value })) }} type='time' placeholder="Reminder " className='rounded-3xl border-stone-200 shadow-3xl placeholder:text-stone-400' />
+                                            </div>
+                                            <div className='flex gap-4 justify-end'>
+                                                <Button className='border border-slate-100 shadow-2xl rounded-3xl h-8 bg-gray-500 text-white hover:bg-gray-800 transition-colors duration-500'>CANCEL</Button>
+                                                <Button onClick={() => { handleEditReminder(opts.eventId) }} className='border border-slate-100 shadow-2xl rounded-3xl h-8 bg-gray-500 text-white hover:bg-gray-800 transition-colors duration-500'>SUBMIT</Button>
+                                            </div>
+                                        </DialogContent>
+                                    </Dialog>
 
-                                        <Dialog>
-                                            <DialogTrigger asChild className='w-full relative'>
-                                                <CiTrash className='cursor-pointer absolute w-full right-0 top-0' />
-                                            </DialogTrigger>
-                                            <DialogContent className='bg-white rounded-xl p-8 shadow-2xl'>
-                                                <span className='text-xl'>Delete Reminder?</span>
-                                                <DialogDescription>
-                                                    This action cannot be undone. This will permanently delete your reminder.
-                                                </DialogDescription>
-                                                <div className='flex gap-4'>
-                                                    <Button onClick={() => { handleDeleteReminder(opts.eventId) }} className='border border-slate-100 shadow-2xl rounded-3xl h-8 bg-gray-500 text-white hover:bg-gray-800 transition-colors duration-500'>YES</Button>
-                                                    <Button className='border border-slate-100 shadow-2xl rounded-3xl h-8 bg-gray-500 text-white hover:bg-gray-800 transition-colors duration-500'>NO</Button>
-                                                </div>
-                                            </DialogContent>
-                                        </Dialog>
-                                    </>
+                                    <Dialog>
+                                        <DialogTrigger asChild className='w-full relative'>
+                                            <CiTrash className='cursor-pointer absolute w-full right-0 top-0' />
+                                        </DialogTrigger>
+                                        <DialogContent className='bg-white rounded-xl p-8 shadow-2xl'>
+                                            <span className='text-xl'>Delete Reminder?</span>
+                                            <DialogDescription>
+                                                This action cannot be undone. This will permanently delete your reminder.
+                                            </DialogDescription>
+                                            <div className='flex gap-4'>
+                                                <Button onClick={() => { handleDeleteReminder(opts.eventId) }} className='border border-slate-100 shadow-2xl rounded-3xl h-8 bg-gray-500 text-white hover:bg-gray-800 transition-colors duration-500'>YES</Button>
+                                                <Button className='border border-slate-100 shadow-2xl rounded-3xl h-8 bg-gray-500 text-white hover:bg-gray-800 transition-colors duration-500'>NO</Button>
+                                            </div>
+                                        </DialogContent>
+                                    </Dialog>
                                 </div></div>
                             <div className='flex flex-col'>
                                 <span className='font-medium'>Desc</span>
