3744a43f76f023367614d14b297410364f1ffa27
robmi10 <89600540+robmi10@users.noreply.github.com>
2024-04-11 23:06:20 +0200

    tweaks

--- app/components/speechrecorder/recorder.tsx
+++ app/components/speechrecorder/recorder.tsx
@@ -2,8 +2,8 @@ import { useReminderStore } from '@/zustand/reminderstore';
 import { useEffect, useRef, useState } from 'react'
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL ?? ''
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_KEY ?? ''
+const supabaseUrl = process.env.SUPABASE_URL ?? ''
+const supabaseAnonKey = process.env.SUPABASE_KEY ?? ''
 
 if (!supabaseUrl || !supabaseAnonKey) {
     throw new Error("Supabase URL and Anon Key must be provided.");
