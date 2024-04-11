ba78700dece68057adecd4e602d044cd318adcf0
robmi10 <89600540+robmi10@users.noreply.github.com>
2024-04-11 21:40:14 +0200

    tweak

--- app/api/storage/route.ts
+++ app/api/storage/route.ts
@@ -6,25 +6,10 @@ const supabaseUrl = process.env.SUPABASE_URL ?? ''
 const supabaseAnonKey = process.env.SUPABASE_KEY ?? ''
 const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
-export const config = {
-    api: {
-        bodyParser: false, // Disables the default Next.js body parser
-    },
-};
 
 export async function POST(req: any) {
-    // const body = await req.json();
-    // const formData = await req.formData();
-    // const selectedaudioBlob = formData.get('audioBlob');
-
-    const form = new formidable.IncomingForm();
-
-
     const blob = await req.formData()
     console.log("formData req blob body->", blob)
-
-    // console.log("body ->", body)
-    // console.log("selectedaudioBlob now ->", selectedaudioBlob)
     let audioBlob = ''
 
     const filePath = `uploads/${Date.now()}-audiofile.wav`;
