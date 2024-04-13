39cdff58ae9f68ae158767d79ecb291aa0322a27
robmi10 <89600540+robmi10@users.noreply.github.com>
2024-04-13 14:10:59 +0200

    tweak

--- app/home/firstpage.tsx
+++ app/home/firstpage.tsx
@@ -6,11 +6,11 @@ import CustomLink from '../components/scroll';
 
 const Firstpage = () => {
     return (
-        <div className="h-screen flex justify-center items-center w-full flex-col gap-8 md:p-0 p-8">
+        <div className="h-screen flex justify-center items-center w-full flex-col gap-4 md:p-0 p-8">
             <div className='absolute top-0 w-full h-full flex items-center justify-center z-0 right-24'>
                 <Globe large={true} start={true} />
             </div>
-            <div className='bg-slate-50 shadow-2xl rounded-3xl p-6 gap-1 flex flex-col z-10 md:w-3/12 items-center'>
+            <div className='bg-slate-50 shadow-2xl mb-8 md:mb-0 rounded-3xl p-6 gap-1 flex flex-col z-10 md:w-3/12 items-center'>
                 <span className="text-xl md:text-2xl flex justify-center z-10" > WELCOME TO AI REMINDER</span>
 
                 <span className="text-sm rounded-2xl z-10 flex justify-center items-center">
@@ -18,7 +18,7 @@ const Firstpage = () => {
                 </span>
             </div>
             <CustomLink href="#about">
-                <IoIosArrowDown className="animate-bounce md:mt-12 cursor-pointer text-stone-50 rounded-full border border-stone-50 p-3 shadow-2xl" size={50} />
+                <IoIosArrowDown className="animate-bounce mb-16 md:mb-0 md:mt-12 cursor-pointer text-stone-50 rounded-full border border-stone-50 p-3 shadow-2xl" size={50} />
             </CustomLink>
         </div>
     )
