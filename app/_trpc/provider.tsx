70b52fbd8bf8c74ad45ba720f12422bb598719e0
robmi10 <89600540+robmi10@users.noreply.github.com>
2024-04-15 14:33:15 +0200

    tweaks

--- app/_trpc/provider.tsx
+++ app/_trpc/provider.tsx
@@ -13,7 +13,7 @@ export default function Provider({ children }: { children: React.ReactNode }) {
                     // url: "http://localhost:3000/api/trpc",
                     // url: "https://ai-reminder.vercel.app/api/trpc",
                     // url: "https://5c12-213-89-153-51.ngrok-free.app/api/trpc",
-                    url: "https://www.aireminder.xyz/"
+                    url: "https://www.aireminder.xyz/api/trpc"
                 }),
             ],
         })
